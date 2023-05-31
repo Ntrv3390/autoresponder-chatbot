f08ce4ef9531bb2cc94d6187655c1ecda97cbab3
Ntrv3390 <78497133+Ntrv3390@users.noreply.github.com>
2023-05-31 12:45:44 +0530

    Second Commit

--- get_bot_message.php
+++ get_bot_message.php
@@ -1,19 +1,58 @@
 <?php
+session_start();
+
+// Set the timezone
 date_default_timezone_set('Asia/Kolkata');
+
+// Include the database configuration
 include('database.inc.php');
-$txt=mysqli_real_escape_string($con,$_POST['txt']);
-$sql="select answer from chats where question like '%$txt%'";
-$res=mysqli_query($con,$sql);
-if(mysqli_num_rows($res)>0){
-	$row=mysqli_fetch_assoc($res);
-	$html=$row['answer'];
-}else{
-	$html="I was not able to understand that query, will try to get an answer for it in the future.";
+$ch = curl_init();
+// Retrieve user input
+$txt = isset($_POST['txt']) ? mysqli_real_escape_string($con, $_POST['txt']) : '';
+
+// Retrieve session ID
+$session_id = $_SESSION['ID'];
+
+
+curl_setopt($ch, CURLOPT_URL, 'https://api.openai.com/v1/completions');
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_POST, 1);
+$postdata=array(
+    "model"=>"text-davinci-001",
+  "prompt"=> $txt,
+  "temperature"=> 0.4,
+  "max_tokens"=> 30,
+  "top_p"=> 1,
+  "frequency_penalty"=> 0,
+  "presence_penalty"=> 0
+);
+$postdata=json_encode($postdata);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $postdata);
+$headers = array();
+$headers[] = 'Content-Type: application/json';
+$headers[] = 'Authorization:Bearer YOUR_OPENAI_API_KEY_HERE';
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+$result = curl_exec($ch);
+if (curl_errno($ch)) {
+    echo 'Error:' . curl_error($ch);
 }
-$added_on=date('Y-m-d h:i:s');
-$session_id = $_SESSION["ID"];
-mysqli_query($con,"insert into message(message,added_on,type,session_id) values('$txt','$added_on','user','$session_id')");
-$added_on=date('Y-m-d h:i:s');
-mysqli_query($con,"insert into message(message,added_on,type,session_id) values('$html','$added_on','bot','$session_id')");
+$result=json_decode($result,true);
+
+$html=$result['choices'][0]['text'];
+curl_close($ch);
+// Insert user and bot messages into the database
+$added_on = date('Y-m-d H:i:s');
+mysqli_query($con, "INSERT INTO message (message, added_on, type, session_id) VALUES ('$txt', '$added_on', 'user', '$session_id')");
+mysqli_query($con, "INSERT INTO message (message, added_on, type, session_id) VALUES ('$html', '$added_on', 'bot', '$session_id')");
+
+// Convert bot's response to audio
+$html_enc = strip_tags($html);
+$html_encoded = htmlspecialchars($html_enc);
+$html_encoded = rawurlencode($html_encoded);
+$html1 = file_get_contents('https://translate.google.com/translate_tts?ie=UTF-8&client=gtx&q=' . $html_encoded . '&tl=en-US');
+$audio_player = "<audio style='display:none' controls='controls' autoplay><source src='data:audio/mpeg;base64," . base64_encode($html1) . "'></audio>";
+
+// Send the response to the client
 echo $html;
+echo $audio_player;
 ?>
